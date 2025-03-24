e73719292f7f13f7768664cdd5608224144eef77
Youngjoon Lee <5462944+youngjoon-lee@users.noreply.github.com>
2025-03-24 09:50:47 +0900

    feat: define Syncman trait

---
+++ core/src/automerge/mod.rs
@@ -0,0 +1,13 @@
+use crate::Syncman;
+
+pub struct SyncmanAutomerge;
+
+impl Syncman for SyncmanAutomerge {
+    fn initiate_sync() {
+        todo!()
+    }
+
+    fn apply_sync() {
+        todo!()
+    }
+}
